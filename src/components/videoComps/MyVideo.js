2d0b38fa929a898ffdda31f7feac52c84bbd8e60
RigvedIntellemo <rigved.srivastava@intellemo.com>
2023-11-08 00:54:18 +0530

    checking account

--- src/components/videoComps/MyVideo.js
+++ src/components/videoComps/MyVideo.js
@@ -41,6 +41,8 @@ const MyPlayer = ({ audioOn, setVideoCompRendered }) => {
         }
     }, [callAccepted])
 
+    console.log('callAccepted', callAccepted);
+
     return (
 
         <div className="row-span-1 col-start-2 col-end-13 md:col-start-1 md:col-end-13 px-5 " id="myVideoDiv">
